7e01b0e870d227bb23571c3a80de30302a249ebd
sorhawell <sorhawell@gmail.com>
2017-03-26 17:33:25 +0200

    dummy package containing persp3d, plot3d and surface3d

---
+++ R/source.R
@@ -0,0 +1,3 @@
+plot3d = function(...) print("rgl::plot3d disabled, reinstall rgl")
+surface3d = function(...) print("rgl::surface3d disabled, reinstall rgl")
+persp3d = function(...) print("rgl::persp3d disabled, reinstall rgl")
